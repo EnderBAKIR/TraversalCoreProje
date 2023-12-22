d677cb30301eb3c005e1bbde7c419157084483b6
Ender Bakır <104258840+EnderBAKIR@users.noreply.github.com>
2023-12-22 14:15:47 +0300

    Update GetAllDestinationQuery.cs

--- TraversalCoreProject/TraversalProject/CQRS/Queries/DestinationQueries/GetAllDestinationQuery.cs
+++ TraversalCoreProject/TraversalProject/CQRS/Queries/DestinationQueries/GetAllDestinationQuery.cs
@@ -1,6 +1,6 @@
 ﻿namespace TraversalProject.CQRS.Queries.DestinationQueries
 {
-    public class GetAllDestinationQuery  // CQRS de isimlendirmeler genelde Get ile başlar
+    public class GetAllDestinationQuery
     {
 
     }
